8bfb03d223ea00342a7506a3de6fcec532b442a6
Akshay Palwe <palweakshay20@gmail.com>
2025-03-27 15:55:39 +0530

    Converted from a module-based structure to a standalone-based architecture.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration(withEventReplay())
-  ],
-  bootstrap: [AppComponent]
+  declarations: [],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration(withEventReplay())],
+  bootstrap: [],
 })
-export class AppModule { }
+export class AppModule {}
